994a49bb0c0c38bd85865288c9333e3fa925a2a9
arunseege <arunsphassan@gmail.com>
2023-04-01 17:40:59 +0530

    enhanced ces to return env details

--- currrency-exchange-service/src/main/java/com/in28minutes/microservices/currrencyexchangeservice/CurrencyExchange.java
+++ currrency-exchange-service/src/main/java/com/in28minutes/microservices/currrencyexchangeservice/CurrencyExchange.java
@@ -1,7 +1,16 @@
 package com.in28minutes.microservices.currrencyexchangeservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class CurrencyExchange {
 	
 	private Long id;
@@ -12,49 +21,5 @@ public class CurrencyExchange {
 
 	private BigDecimal conversionMultiple;
 
-	public CurrencyExchange() {
-		
-	}
-	
-	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
-		super();
-		this.id = id;
-		this.from = from;
-		this.to = to;
-		this.conversionMultiple = conversionMultiple;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFrom() {
-		return from;
-	}
-
-	public void setFrom(String from) {
-		this.from = from;
-	}
-
-	public String getTo() {
-		return to;
-	}
-
-	public void setTo(String to) {
-		this.to = to;
-	}
-
-	public BigDecimal getConversionMultiple() {
-		return conversionMultiple;
-	}
-
-	public void setConversionMultiple(BigDecimal conversionMultiple) {
-		this.conversionMultiple = conversionMultiple;
-	}
-
-	
+	private String environment;
 }
--- currrency-exchange-service/src/main/java/com/in28minutes/microservices/currrencyexchangeservice/CurrencyExchangeController.java
+++ currrency-exchange-service/src/main/java/com/in28minutes/microservices/currrencyexchangeservice/CurrencyExchangeController.java
@@ -2,19 +2,23 @@ package com.in28minutes.microservices.currrencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-		
+		@Autowired
+		private Environment environment;
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from,
 			@PathVariable String to) {
 		return new CurrencyExchange(1000L, from, to, 
-						BigDecimal.valueOf(50));
+						BigDecimal.valueOf(50),environment.getProperty("local.server.port"));
 		
 	}
 
